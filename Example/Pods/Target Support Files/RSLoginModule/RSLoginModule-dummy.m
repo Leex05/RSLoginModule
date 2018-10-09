464044925f573c667602022a6300a40df2d260da
leex <leixiao@kaituocn.com>
2018-10-09 14:16:40 +0800

    登陆组件

---
+++ Example/Pods/Target Support Files/RSLoginModule/RSLoginModule-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_RSLoginModule : NSObject
+@end
+@implementation PodsDummy_RSLoginModule
+@end
---
+++ Example/Pods/Target Support Files/RSLoginModule/RSLoginModule-umbrella.h
@@ -0,0 +1,24 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "LoginViewController.h"
+#import "PromoCodeController.h"
+#import "PromoDetailsController.h"
+#import "ProperyController.h"
+#import "RegistViewController.h"
+#import "ResetPasswordController.h"
+#import "CTMediator+Login.h"
+#import "Target_Login.h"
+
+FOUNDATION_EXPORT double RSLoginModuleVersionNumber;
+FOUNDATION_EXPORT const unsigned char RSLoginModuleVersionString[];
+
